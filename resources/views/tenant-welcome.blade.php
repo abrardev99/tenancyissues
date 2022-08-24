a574e861f64e73b6c09d87d3b00b0ddd4ad44065
Abrar Ahmad <abrar.dev99@gmail.com>
2022-08-24 17:05:35 +0500

    Update tenant-welcome.blade.php

--- resources/views/tenant-welcome.blade.php
+++ resources/views/tenant-welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    {{ dd(session()); }}
+                    {{ dd(session()) }}
                     @if(session()->has('tenancy_impersonated_by'))
                         You are impersonating as: {{ session()->get('tenancy_impersonated_by') }}
 
