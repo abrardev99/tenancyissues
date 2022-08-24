09732428d0a0a169e7720b73f962606217ad6843
Abrar Ahmad <abrar.dev99@gmail.com>
2022-08-24 17:05:22 +0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tenant;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/imper', function (){
+    $tenant = Tenant::first();
+    $domain = $tenant->domains->first()->domain;
+    $domain = $domain . '.' . config('tenancy.central_domains')[0];
+
+    $token = tenancy()->impersonate($tenant, 1, 'http://foo.' . config('tenancy.central_domains')[0], 'web')->token;
+
+    return redirect("http://$domain/impersonate/{$token}");
 });
